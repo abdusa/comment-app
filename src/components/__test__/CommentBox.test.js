776bc24fb931d3798da1fd3a64cce9f7b3032ee9
Abdusa Alam <abdusa.alam@gmail.com>
2018-06-10 04:01:20 +0700

    Write testcase for CommentBox component

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,3 +1,43 @@
-import react from 'react';
-
+import React from 'react';
+import { mount } from 'enzyme';
 import CommentBox from '../CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => wrapped.unmount());
+
+it('Has a textarea and a button element', () => {
+
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+
+/* Group tests using describe */
+describe('Handle Textarea', () => {
+
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: {
+        value: 'new comment',
+      }
+    });
+
+    wrapped.update(); /* Force update component */
+  });
+
+  it('Has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+  
+  it('input is submitted, textarea should get emptied', () => {  
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+  
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
